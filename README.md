187d4cea6ce6cc8de078b4ab1e460a7bbca074d7
Mateus Conceição <fera7775@yahoo.com>
2020-06-18 10:18:51 -0300

    Maiis um desafio concluido, dessa vez a parte do cliente utilizando Reactjs+Axios para consumir a api do desafio 02

---
+++ README.md
@@ -0,0 +1,53 @@
+![goStack](.github/gostack.png)
+<h1 align="center">
+  🚀 Conceitos ReactJS 🚀
+  <p align="center">
+  <img src="https://img.shields.io/badge/tech-front--end-green" />
+
+  <a href="https://reactjs.org/">
+    <img src="https://img.shields.io/badge/framework-reactjs-blue" />
+  </a>
+
+  <a href="https://github.com/Rocketseat">
+    <img src="https://img.shields.io/badge/source-rocketseat-blueviolet" />
+  </a>
+  </p>
+</h1>
+<p>
+  Fala Devs esse é o front-end da api construida <a href="https://github.com/fera765/Gostack-desafio-02">desafio anterior</a>, é possível criar, remover, e dar likes em cada repositório..
+</p>
+
+<h2 align="center">
+  Sobre 🤓
+</h2>
+
+<p>
+  Esse projeto tem como conceitos, Componentização dentro do React, Propriedades e os conceitos de Estado e Imutabilidade!!
+</p>
+
+<h2 align="center">
+  Para rodar o projeto
+</h2>
+
+```bash
+  #  clone o repositório
+  git clone https://github.com/fera765/Gostack-desafio-front-end-03.git
+
+  #  navegue até o repositório clonado
+  cd Gostack-desafio-front-end-03
+
+  #  baixe as dependências
+  yarn
+
+  #  divirta-se!
+  yarn start
+```
+<p align="center">
+  ~💜  Linkedin: <a href="https://www.linkedin.com/in/lord775/">Mateus Conceição</a>
+</p>
+
+<p align="center">
+  Deixe uma ⭐ para que mais devs cheguem por aqui! 🚀
+</p>
+
+<hr>
--- src/App.js
+++ src/App.js
@@ -1,29 +1,63 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    });
+  },[])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+          "title": `Nodejs Adonisjs ${Date.now()}`,
+          "url": "https:github.com",
+          "techs": [
+            "eslint",
+            "prettier"
+          ]
+    })
+
+    setRepositories([response.data, ...repositories])
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  const handleRemoveRepository = useCallback(
+    (id) => {
+      api.delete(`/repositories/${id}`);
+
+      setRepositories((repo) =>
+        repo.filter((repository) => repository.id !== id)
+      );
+    },
+    [setRepositories]
+  );
+  
+  async function handleLikesRepository(id){
+    const response = await api.post(`repositories/${id}/like`);
+    const responseLike = response.data;
+    setRepositories(repositories.map(repository => repository.id === responseLike.id ? repository = responseLike :repository))
   }
 
   return (
     <div>
+      <button onClick={handleAddRepository}>Adicionar</button>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+       {repositories.map(repository => (  
+        <li key={repository.id}>
+          {repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
+          <button className="like" onClick={() => handleLikesRepository(repository.id)}>
+          <small>{repository.likes}</small> {repository.likes > 0 ? '🚀' : ''} Like
+          </button>
         </li>
+       ))}
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
